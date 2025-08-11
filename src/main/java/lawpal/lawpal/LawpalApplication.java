f58546da82c47743ec613da33f07df07e12f8581
mj-song00 <niveus128@gmail.com>
2025-08-11 21:43:42 +0900

    init message

---
+++ src/main/java/lawpal/lawpal/LawpalApplication.java
@@ -0,0 +1,13 @@
+package lawpal.lawpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawpalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawpalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lawpal/lawpal/LawpalApplicationTests.java
@@ -0,0 +1,13 @@
+package lawpal.lawpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawpalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
