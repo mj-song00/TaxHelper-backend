d496c325337d51e75554359447c558c5fb04a1fc
mj-song00 <niveus128@gmail.com>
2026-05-12 15:53:37 +0900

    Rename: jwt 폴더 변경 외

--- src/main/java/lawpal/lawpal/common/filter/FilterConfig.java
+++ src/main/java/lawpal/lawpal/common/filter/FilterConfig.java
@@ -1,4 +1,4 @@
-package lawpal.lawpal.filter;
+package lawpal.lawpal.common.filter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
