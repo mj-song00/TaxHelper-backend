b359355baeb397152669b9da34ad79981ff92701
mj-song00 <niveus128@gmail.com>
2026-05-20 11:17:54 +0900

    rename(app): application 이름 변경

--- src/main/java/lawpal/lawpal/TaxhelperApplication.java
+++ src/main/java/lawpal/lawpal/TaxhelperApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LawpalApplication {
+public class TaxhelperApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LawpalApplication.class, args);
+        SpringApplication.run(TaxhelperApplication.class, args);
     }
 
 }
