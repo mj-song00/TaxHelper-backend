c671c109610530f67b02f1b3d57147a7c7c13707
mj-song00 <niveus128@gmail.com>
2026-05-26 18:26:48 +0900

    feat(ministry): 부처 repository 생성

---
+++ src/main/java/lawpal/lawpal/domain/ministry/repository/MinistryRepository.java
@@ -0,0 +1,10 @@
+package lawpal.lawpal.domain.ministry.repository;
+
+import lawpal.lawpal.domain.ministry.entity.Ministry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MinistryRepository extends JpaRepository<Ministry, Long> {
+    Optional<Ministry> findByMinistryCode(String ministryCode);
+}
