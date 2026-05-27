fab8c3ffb07acc3131833586b891b6bb7ff7d79c
mj-song00 <niveus128@gmail.com>
2026-05-27 17:47:22 +0900

    fix(law): law 저장시 중복으로 저장되는 ministry 수정

--- src/main/java/lawpal/lawpal/domain/ministry/repository/MinistryRepository.java
+++ src/main/java/lawpal/lawpal/domain/ministry/repository/MinistryRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MinistryRepository extends JpaRepository<Ministry, Long> {
-    Optional<Ministry> findByMinistryCode(String ministryCode);
+    Optional<Ministry> findFirstByMinistryCode(String ministryCode);
 }
