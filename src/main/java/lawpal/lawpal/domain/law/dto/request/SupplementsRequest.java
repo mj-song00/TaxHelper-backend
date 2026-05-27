439999b54230afc2e29335bd951fc7f61f184ee8
mj-song00 <niveus128@gmail.com>
2026-05-27 17:47:47 +0900

    fix(law): 주석 제거

--- src/main/java/lawpal/lawpal/domain/law/dto/request/SupplementsRequest.java
+++ src/main/java/lawpal/lawpal/domain/law/dto/request/SupplementsRequest.java
@@ -1,13 +1,11 @@
 package lawpal.lawpal.domain.law.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lawpal.lawpal.domain.law.entity.LawSupplement;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -17,10 +15,10 @@ public class SupplementsRequest {
     @JsonProperty("부칙단위")
     private List<SupplementUnitRequest> units;
 
-    public List<LawSupplement> toEntityList() {
-        if (units == null) return null;
-        return units.stream()
-                .map(SupplementUnitRequest::toEntity)
-                .collect(Collectors.toList());
-    }
+//    public List<LawSupplement> toEntityList() {
+//        if (units == null) return null;
+//        return units.stream()
+//                .map(SupplementUnitRequest::toEntity)
+//                .collect(Collectors.toList());
+//    }
 }
