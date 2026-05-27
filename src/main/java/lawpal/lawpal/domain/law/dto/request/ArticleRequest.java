2684741cb14e335c462b63bed1fc7e663aae727c
mj-song00 <niveus128@gmail.com>
2026-05-27 17:50:49 +0900

    fix(article): 조문 request dto 주석 제거

--- src/main/java/lawpal/lawpal/domain/law/dto/request/ArticleRequest.java
+++ src/main/java/lawpal/lawpal/domain/law/dto/request/ArticleRequest.java
@@ -1,12 +1,9 @@
 package lawpal.lawpal.domain.law.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lawpal.lawpal.domain.law.entity.LawArticle;
 import lombok.Getter;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 public class ArticleRequest {
@@ -14,10 +11,10 @@ public class ArticleRequest {
     @JsonProperty("조문단위")
     private List<ArticleUnitRequest> units;
 
-    public List<LawArticle> toEntityList() {
-        if (units == null) return Collections.emptyList();
-        return units.stream()
-                .map(ArticleUnitRequest::toEntity)
-                .collect(Collectors.toList());
-    }
+//    public List<LawArticle> toEntityList() {
+//        if (units == null) return Collections.emptyList();
+//        return units.stream()
+//                .map(ArticleUnitRequest::toEntity)
+//                .collect(Collectors.toList());
+//    }
 }
