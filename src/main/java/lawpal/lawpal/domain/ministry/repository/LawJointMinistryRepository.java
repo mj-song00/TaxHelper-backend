f91487be9bb0fa9922f5340b5c21921b30988b7c
mj-song00 <niveus128@gmail.com>
2026-05-27 17:48:51 +0900

    feat(ministry): 2개이상의 공동부처를 저장하기 위한 repository 생성

---
+++ src/main/java/lawpal/lawpal/domain/ministry/repository/LawJointMinistryRepository.java
@@ -0,0 +1,7 @@
+package lawpal.lawpal.domain.ministry.repository;
+
+import lawpal.lawpal.domain.ministry.entity.LawJointMinistry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LawJointMinistryRepository extends JpaRepository<LawJointMinistry, Long> {
+}
