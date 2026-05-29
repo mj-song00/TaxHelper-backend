eddd7be89f529f01565d5899a93d7087d1b99b47
mj-song00 <niveus128@gmail.com>
2026-05-29 16:20:01 +0900

    chroe(article): 주석제거

--- src/main/java/lawpal/lawpal/domain/law/dto/request/ArticleRequest.java
+++ src/main/java/lawpal/lawpal/domain/law/dto/request/ArticleRequest.java
@@ -10,11 +10,4 @@ public class ArticleRequest {
 
     @JsonProperty("조문단위")
     private List<ArticleUnitRequest> units;
-
-//    public List<LawArticle> toEntityList() {
-//        if (units == null) return Collections.emptyList();
-//        return units.stream()
-//                .map(ArticleUnitRequest::toEntity)
-//                .collect(Collectors.toList());
-//    }
 }
