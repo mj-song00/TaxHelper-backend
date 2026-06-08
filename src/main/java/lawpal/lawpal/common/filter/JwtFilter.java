63fa7e2552f5b5058bc881311e5c6c75ffdb7165
mj-song00 <niveus128@gmail.com>
2026-06-08 11:18:22 +0900

    chore(app): 판례목록 조회 화이트리스트 추가

--- src/main/java/lawpal/lawpal/common/filter/JwtFilter.java
+++ src/main/java/lawpal/lawpal/common/filter/JwtFilter.java
@@ -72,6 +72,7 @@ public class JwtFilter  implements Filter {
                 || (url.startsWith("/api/v1/coin/price"))
                 || (url.startsWith("/api/v1/law"))
                 || (url.startsWith("/api/laws/chunks"))
+                || (url.startsWith("/api/v1/precedent"))
         ) {
             chain.doFilter(request, response);
             return;
